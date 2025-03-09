86d712f342429737bd0a6b22dc5ebeda0e0e7f7c
Maic <maic1405@gmail.com>
2025-03-09 15:08:42 -0300

    FEAT: add estilos e assets da aplicação

---
+++ components/theme.js
@@ -0,0 +1,17 @@
+export const COLORS = {
+    red: '#ed5359',
+    dark_gray: '#5d6275',
+    medium_gray: '#a6abbc',
+    light_gray: '#e5ecfc',
+    white: '#f4f7fe',
+    green: '#38c682'
+}
+
+export const FONT_SIZE = {
+    xsm: 11,
+    sm: 14,
+    md: 16,
+    lg: 22,
+    xl: 32,
+    xxl: 64
+}
